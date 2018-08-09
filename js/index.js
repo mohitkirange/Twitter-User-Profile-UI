dbe6b55a625e15d71830fcd35e10583a47a93d12
mohitkirange <mohitk@csu.fullerton.edu>
2018-08-09 16:15:55 -0700

    Twitter User Profile UI

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+$(document).ready(
+
+	function iniciar(){
+	$('.follow').on("click", function(){
+		$('.follow').css('background-color','#144C72');
+		$('.follow').html('<div class="icon-ok"></div> Following');
+	});	
+	}
+
+);
